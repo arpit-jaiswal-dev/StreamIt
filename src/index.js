2352b31a49f93175dbd1ad7445b7698e251bfe31
Arpit Jaiswal <arpit.18jaiswal@gmail.com>
2026-08-02 15:23:52 +0530

    connection to database

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // require("dotenv").config({path: "./.env"}); 
 import dotenv from "dotenv";
-dotenv.config({path: "./.env"});
+dotenv.config({path: "./env"});
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants.js";
 import DbConnect from "./db/index.js";
+import app from './app.js'
 // ;( async ()=>{
 //     try{
 //         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`) 
@@ -21,4 +22,13 @@ import DbConnect from "./db/index.js";
 //     }
 // })()
 
-DbConnect();
+DbConnect()
+.then(()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB Connection failed!! : ",err);
+    process.exit(1);
+})
