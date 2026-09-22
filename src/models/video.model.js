61c24e4c2bdb7e1db7b4191b8f5ae6984f3bd010
Arpit Jaiswal <arpit.18jaiswal@gmail.com>
2026-09-22 10:19:52 +0530

    xyz

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import { type } from "express/lib/response";
+import mongoose,{Schema, trusted} from "mongoose";
+import { User } from "./user.model";
+
+const videoSchema = new Schema(
+    {
+        videoFile:{
+            type:String,
+            required:true
+        },
+        thumbnail:{
+            type:String,
+            required:true
+        },
+        title:{
+            type:String,
+            required:true
+        },
+        description:{
+            type:String,
+            required:true
+        },
+        duration:{
+            type:Number,
+            required:true
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        isPulished:{
+            type:Boolean,
+            default:true
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+export const Video = mongoose.model("Video",videoSchema)
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,11 +1,12 @@
-class ApiError extends Error{
+class ApiError extends Error{ // extends Error means ApiError inherits all the features of JavaScript's built-in Error class
     constructor(
-        statusCode,
-        message="Something Went Wrong",
-        errors = [],
-        statck = ""
+       statusCode,
+       message="Something went wrong",
+       errors = [],
+       statck = ""
+
     ){
-        super(message)
+        super(message) 
         this.statusCode = statusCode
         this.data = null
         this.message = message
