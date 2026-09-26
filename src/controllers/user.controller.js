54b5ab723ac3eb386e8bf2f55b34180513670211
Arpit Jaiswal <arpit.18jaiswal@gmail.com>
2026-09-26 19:25:06 +0530

    xyz

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,40 @@
-import {asyncHandler} from '../utils/asyncHandler.js'
-
+import asyncHandler from '../utils/asyncHandler.js'
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
 const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message:"ok"
+    // get user details from frontend
+    // validation of details - non empty
+    // check if user already exists
+    // check for images
+    // check for avatar
+    // upload to cloudinary
+    // create user object - create entry in db 
+    // remove password and refresh token field from respose
+    // check for user creation
+    // return res
+
+    const {fullname,email,username,password} = req.body;
+    console.log(fullname)
+    // if(fullname === ""){
+    //     throw new ApiError(400,"Full name is required")
+    // }
+
+    if(
+        [fullname,email,username,password].some((field)=>
+        field?.trim() === "")
+    ){
+        throw new ApiError(400,"All fields is required")
+    }
+
+    const existedUser = User.findOne({
+        $or:[{username},{email}]
     })
+
+    if(existedUser){
+        throw new ApiError(409,"User already exists with this email or username")
+    }
+
+    req.files?.avatar[0]
 }) 
 
 export {registerUser}
---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,17 @@
+import multer  from "multer";
+import crypto from "crypto";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp")
+  },
+  filename: function (req, file, cb) {
+    // crypto.randomBytes(16, function (err, raw) {
+    //   if (err) return cb(err)
+      cb(null, file.originalname )
+    // })
+  }
+})
+
+const upload = multer({ storage: storage })
+export default upload;
